cf1798ee3a38daf6e86ae18b851522eab5d48172
meteo04 <jens-duffert@t-online.de>
2015-11-13 21:50:25 +0100

    Aufgabe4

--- assignment04/volume.c
+++ assignment04/volume.c
@@ -14,8 +14,8 @@ const double pi = 3.14; // Die Zahl PI welche für die Berechnung des Kreises wi
 double vol; // hilfsvariable, wenn das volumen negativ
 
 enum Volumenversch {
-    TVolumenQuarderEuclid, // tag for a Volumen in Euclidean coordinates
-    TVolumenKugelPolar,   // tag for a Volumen in polar coordinates
+    TVolumenQuarderEuclid, // 
+    TVolumenKugelPolar,   // 
 	TVolumenZylinderPolar // 
 };
 
@@ -26,18 +26,16 @@ struct VolumenQuarderEuclid {
 };
 
 struct VolumenKugelPolar {
-    double PhiKugel;
     double RadiusKugel;
 };
 
 struct VolumenZylinderPolar {
-    double PhiZylinder;
     double RadiusZylinder;
 	double HoeheZylinder;
 };
 
 struct Volumen {
-    enum Volumenversch tag;                // indicate which variant follows
+    enum Volumenversch tag;                // 
     union {
 		struct VolumenQuarderEuclid euclid; // Euclidean variant
 		struct VolumenKugelPolar polar;      // polar variant
@@ -45,8 +43,8 @@ struct Volumen {
     };
 };
 
-// constructor for VolumenEuclid
-struct Volumen make_Volumen_euclid(double x, double y, double z) {
+// constructor für VolumenQuader
+struct Volumen make_VolumenQuarder_euclid(double x, double y, double z) {
     struct Volumen p;
     p.tag = TVolumenQuarderEuclid;
     p.euclid.x = x;
@@ -55,7 +53,7 @@ struct Volumen make_Volumen_euclid(double x, double y, double z) {
     return p;
 }
 
-// constructor for VolumenPolar
+// constructor für VolumenKugel
 struct Volumen make_VolumenKugel_polar(double rku) {
     struct Volumen p;
     p.tag = TVolumenKugelPolar;
@@ -63,23 +61,22 @@ struct Volumen make_VolumenKugel_polar(double rku) {
     return p;
 }
 
-// constructor for VolumenPolar
-struct Volumen make_VolumenZylinder_polare(double rzy, double hzy) {
+// constructor für VolumenZylinder
+struct Volumen make_VolumenZylinder_polare(double r, double h) {
     struct Volumen p;
     p.tag = TVolumenKugelPolar;
-    p.polare.RadiusZylinder = rzy;
-	p.polare.HoeheZylinder = hzy;
+    p.polare.RadiusZylinder = r;
+	p.polare.HoeheZylinder = h;
     return p;
 }
 
 
-// struct Volumen -> double
-// Computes the distance from the given Volumen
-// to the origin of the coordinate system.
+// 
 double volume(struct Volumen p);
 
+
 static void volume_test() {
-    // test cases for polar variant
+    // test für die Kugel (Radius)
     check_within_d(
         volume(make_VolumenKugel_polar(0.0)), 
         0.0, EPSILON);
@@ -88,34 +85,35 @@ static void volume_test() {
         3.14, EPSILON);
     check_within_d(
         volume(make_VolumenKugel_polar(2.0)), 
-        12.56, EPSILON);
+        25.12, EPSILON);
 
-    // test cases for Euclidean variant
+    // test für den Quarder (x, y, z)
     check_within_d(
-        volume(make_Volumen_euclid(-2.0, 2.0)), 
+        volume(make_VolumenQuarder_euclid(0.0, -2.0, 2.0)), 
         0.0, EPSILON);
     check_within_d( 
-        volume(make_Volumen_euclid(-2.0, 2.0)), 
+        volume(make_VolumenQuarder_euclid(2.0, -2.0, 2.0)), 
         8.0, EPSILON);
     check_within_d( 
-        volume(make_Volumen_euclid(1.0, 1.0)), 
-        1.0, EPSILON); // square root
+        volume(make_VolumenQuarder_euclid(1.0, 1.0, 1.0)), 
+        1.0, EPSILON); 
 	
 	
-	// test für den Zylinder	
+	// test für den Zylinder (Radius, Hoehe)	
 	check_within_d(
-        volume(make_VolumenZylinder_polare(0.0, 1.0, 2.0)), 
+        volume(make_VolumenZylinder_polare(3.0, 2.0)), 
+        56.52, EPSILON);
+    check_within_d(
+         volume(make_VolumenZylinder_polare(1.0, 2.0)), 
         6.28, EPSILON);
     check_within_d(
-         volume(make_VolumenZylinder_polare(0.0, 1.0, 2.0)), 
-        6.28, EPSILON);
-    check_within_d(
-        volume(make_VolumenZylinder_polare(2.0, 2.0, 2.0)), 
-        25.12, EPSILON);	
+        volume(make_VolumenZylinder_polare(0.5, 2.0)), 
+        1.57, EPSILON);	
 }
 
-// Computes the distance from the given Volumen
-// to the origin of the coordinate system.
+
+
+// Berechnet das Volumen der einzelnen Körper
 double volume(struct Volumen p) {
     if (p.tag==TVolumenQuarderEuclid) {
         vol = p.euclid.x * p.euclid.y * p.euclid.z;
@@ -127,11 +125,21 @@ double volume(struct Volumen p) {
 	}	
 	
 	if (p.tag==TVolumenKugelPolar){
-       return (4/3)*p.polar.RadiusKugel*p.polar.RadiusKugel* pi;
+       vol = (4/3)*p.polar.RadiusKugel*p.polar.RadiusKugel*p.polar.RadiusKugel* pi;
+	   if (vol < 0){
+		   return vol * (-1);
+	   } else {
+		   return vol;
+	   }
     }
 	
 	if (p.tag==TVolumenZylinderPolar){
-		return p.polare.RadiusZylinder * p.polare.RadiusZylinder * pi * p.polare.HoeheZylinder;
+		vol = p.polare.RadiusZylinder * p.polare.RadiusZylinder * p.polare.HoeheZylinder * pi;
+		if (vol < 0){
+			return vol * (-1);
+		} else {
+			return vol;
+		}
 	}
     return 0;
 	
