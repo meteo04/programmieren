6d6699215fcc89bdf60d869995e859d2d3e5c0e5
TimKleinfeld <tim-kleinfeld@live.de>
2015-11-27 19:18:31 +0100

    Update Gewichtsumrechnung.c

--- Gewichtsumrechnung.c
+++ Gewichtsumrechnung.c
@@ -29,16 +29,18 @@ B:
 */
 Array total_weight_kg(String); 
 
+const double epsilon = 0.000000000000001;
+
 
 /*
 D:
 Test-Arrays
 */
 static void total_weight_kg_test(void) {
-    check_within_d(total_weight_kg(sa_of_String("10", "kg", "500", "g", "0.5", "t")), 10 + 0.5 + 500); //Vorgegebenes Bsp.
-	check_within_d(total_weight_kg(sa_of_String("8000", "g", "20", "kg", "0.04", "t")), 8 + 20 + 40); //Bsp:1
-	check_within_d(total_weight_kg(sa_of_String("5", "t", "0.4", "t", "23000", "g")), 5000 + 400 + 23); //Bsp:2
-	check_within_d(total_weight_kg(sa_of_String("500", "g", "800", "g", "0.9", "kg")), 0.5 + 0.8 + 0.9); //Bsp:3
+    check_within_d(total_weight_kg(sa_of_string("10, kg, 500, g, 0.5, t")), 10 + 0.5 + 500, epsilon); //Vorgegebenes Bsp.
+	check_within_d(total_weight_kg(sa_of_string("8000, g, 20, kg, 0.04, t")), 8 + 20 + 40, epsilon); //Bsp:1
+	check_within_d(total_weight_kg(sa_of_string("5, t, 0.4, t, 23000, g")), 5000 + 400 + 23, epsilon); //Bsp:2
+	check_within_d(total_weight_kg(sa_of_string("500, g, 800, g, 0.9, kg")), 0.5 + 0.8 + 0.9, epsilon); //Bsp:3
 
 }
 
@@ -69,7 +71,7 @@ Bemerkung: Noch nicht ganz klar!
 */
 double total_weight_kg(Array a) { 
 	
-	int n = a_lenght(a);
+	int n = a_length(a);
 	double all = 0;
 	
 	for(int x = 0; x < n; x = x + 2) {
